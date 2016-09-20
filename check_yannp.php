381e1e2e40d7343dc8e73726423a1c481373e44d
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-09-20 14:58:46 +0100

    Add templates

---
+++ check_yannp.php
@@ -0,0 +1,5 @@
+<?php
+# Need to write a pnp4nagios template that accepts the basic inputs as shown in check_ping.php
+# but can handle multiple graphs on one set of axes as in check_openmanage.php, see the Chassis Temperatures source.
+# https://status.resnet.bris.ac.uk/pnp4nagios/graph?host=db2.nomadic-core.bris.ac.uk&srv=Dell_OpenManage&view=0
+?>
