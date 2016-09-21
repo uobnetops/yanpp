451a5f49e4762373fae55da3302815b090732067
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-09-21 14:14:58 +0000

    Fix display of second graph

--- check_yanpp.php
+++ check_yanpp.php
@@ -32,18 +32,18 @@ $opt[2] = "--vertical-label \"Packets lost\" -l0 -u105 --title \"Packets lost\" 
 
 $def[2]  =  rrd::def("var1", $RRDFILE[2], $DS[5], "AVERAGE");
 $def[2] .=  rrd::gradient("var1", "ff5c00", "ffdc00", "Packets Lost", 20) ;
-$def[2] .=  rrd::gprint("var1", array("LAST", "MAX", "AVERAGE"), "%3.0lf $UNIT[2]") ;
+$def[2] .=  rrd::gprint("var1", array("LAST", "MAX", "AVERAGE"), "%3.0lf $UNIT[5]") ;
 $def[2] .=  rrd::line1("var1", "#000000") ;
 
 $def[2] .= rrd::hrule("100", "#000000") ;
 
-if($WARN[2] != ""){
-        if($UNIT[2] == "%%"){ $UNIT[2] = "%"; };
-        $def[2] .= rrd::hrule($WARN[2], "#FFFF00", "Warning  ".$WARN[2].$UNIT[2]."\\n");
+if($WARN[5] != ""){
+        if($UNIT[5] == "%%"){ $UNIT[5] = "%"; };
+        $def[2] .= rrd::hrule($WARN[5], "#FFFF00", "Warning  ".$WARN[5].$UNIT[5]."\\n");
 }
-if($CRIT[2] != ""){
-        if($UNIT[2] == "%%"){ $UNIT[2] = "%"; };
-        $def[2] .= rrd::hrule($CRIT[2], "#FF0000", "Critical ".$CRIT[2].$UNIT[2]."\\n");
+if($CRIT[5] != ""){
+        if($UNIT[5] == "%%"){ $UNIT[5] = "%"; };
+        $def[2] .= rrd::hrule($CRIT[5], "#FF0000", "Critical ".$CRIT[5].$UNIT[5]."\\n");
 }
 
 ?>
